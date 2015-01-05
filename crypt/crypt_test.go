c06d028ce6f2b290350b6cec29e7a2c3deaf5b12
Rob Thornton <rthornton128@gmail.com>
2015-01-04 20:46:55 -0800

    Add crypt test

--- crypt/crypt_test.go
+++ crypt/crypt_test.go
@@ -10,7 +10,16 @@ func TestValidPassword(t *testing.T) {
 	raw_password := "password"
 	hashed_password, salt := crypt.Hash(raw_password)
 
-	if ok := crypt.Validate(raw_password, hashed_password, salt); !ok {
+	if !crypt.Validate(raw_password, hashed_password, salt) {
+		t.FailNow()
+	}
+}
+
+func TestInvalidPassword(t *testing.T) {
+	raw_password := "password"
+	hashed_password, salt := crypt.Hash(raw_password)
+
+	if crypt.Validate("nope", hashed_password, salt) {
 		t.FailNow()
 	}
 }
