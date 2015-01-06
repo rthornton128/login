b93ca799d5fe8bf577b9fb753ac91be4d957af32
Rob Thornton <rthornton128@gmail.com>
2015-01-05 20:05:06 -0800

    Import middle package

---
+++ middle/middle.go
@@ -0,0 +1,21 @@
+/*
+Package middle is an example of what middleware might look like */
+package middle
+
+import (
+	"log"
+	"net/http"
+)
+
+/*
+LogAccess logs the user ID of the person trying to login along with their
+IP address */
+func LogAccess(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			log.Println("Attempted login by:", r.FormValue("UserID"),
+				"("+r.RemoteAddr+")")
+		}
+		f(w, r)
+	}
+}
