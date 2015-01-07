24642065b5cdbb14b2940531e3d0fee7595f7bb5
Rob Thornton <rthornton128@gmail.com>
2015-01-06 17:02:44 -0800

    change comment style and use more idiomatic variable names

--- crypt/crypt.go
+++ crypt/crypt.go
@@ -1,6 +1,6 @@
-/* Crypto implements a simple password encryption method. bcrypt would be
- * better in a production environment but crypto/rand will more suffice for
- * this example (and probably even in many live programs, too) */
+// Package crypt implements a simple password encryption method. bcrypt would
+// be better in a production environment but crypto/rand will more suffice for
+// this example (and probably even in many live programs, too)
 package crypt
 
 import (
@@ -10,24 +10,24 @@ import (
 	"log"
 )
 
-/* Hash generates a sha512 password hash and a sha512.Size salt */
-func Hash(raw_password string) (password, salt []byte) {
-	/* generate salt (uses /dev/urandom on Linux). Only a half-hearted attempt
-	 * is made to track an error. Note that Read can return less than 16 bytes,
-	 * which would make for a poor salt, but is not checked */
+// Hash generates a sha512 password hash and a sha512.Size salt
+func Hash(rawPassword string) (password, salt []byte) {
+	// generate salt (uses /dev/urandom on Linux). Only a half-hearted attempt
+	// is made to track an error. Note that Read can return less than 16 bytes,
+	// which would make for a poor salt, but is not checked
 	salt = make([]byte, sha512.Size)
 	if _, err := rand.Read(salt); err != nil {
 		log.Println("new salt:", err)
 	}
 
-	/* hash the password using salt and return both values */
-	hashed_password := sha512.Sum512([]byte(raw_password + string(salt)))
-	return hashed_password[:], salt
+	// hash the password using salt and return both values
+	hashedPassword := sha512.Sum512([]byte(rawPassword + string(salt)))
+	return hashedPassword[:], salt
 }
 
-/* Validate returns true if the supplied plain text password hashes to
- * the supplied hashed password and salt combination; otherwise, false */
-func Validate(raw_password string, hashed_password, salt []byte) bool {
-	hashed_result := sha512.Sum512([]byte(raw_password + string(salt)))
-	return bytes.Equal(hashed_result[:], hashed_password)
+// Validate returns true if the supplied plain text password hashes to
+// the supplied hashed password and salt combination; otherwise, false
+func Validate(rawPassword string, hashedPassword, salt []byte) bool {
+	hashedResult := sha512.Sum512([]byte(rawPassword + string(salt)))
+	return bytes.Equal(hashedResult[:], hashedPassword)
 }
